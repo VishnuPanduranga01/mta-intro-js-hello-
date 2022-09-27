adac96bfeb94866ffc35226a300ba8ccd163e1ee
Vishnu Panduranga <>
2022-09-27 19:15:21 +0530

    Initial commit

---
+++ app1/main.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.get('/', function (req, res) {
+
+    var page = "<!DOCTYPE html><html><body>"
+        + "<h1>Hello Node! test SAP</h1>"
+        + "</body></html>";
+
+    res.send(page);
+});
+
+const port = process.env.PORT || 3002;
+app.listen(port);
